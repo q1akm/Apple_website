e459a23728e231015de6eb7ad0cb48550af71d6c
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-04-25 15:49:59 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6 w-full flex-col flex-center">
+          <p className="hero-title">IPHONE 15 PRO</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
