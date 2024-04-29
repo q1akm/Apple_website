6886eba5f69e1191304dc604cba7972301e0ae9b
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-04-29 22:15:11 +0700

    adding some lights

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,12 +4,15 @@ import ModelView from "./ModelView"
 import { useRef, useState } from "react";
 import * as THREE from 'three';
 import {yellowImg} from '../utils'
+import { View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import {models, sizes} from '../constants'
 
 const Model = () => {
 
     const [size, setSize] = useState('small');
     const [model, setModel] = useState({
-        titl: 'Iphone 15 pro in Natural Titanium',
+        title: 'Iphone 15 pro in Natural Titanium',
         color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
         img: yellowImg,
     })
@@ -57,8 +60,60 @@ const Model = () => {
                    setRotationState={setSmallRotation}
                    item={model}
                    size={size}/>
+
+                   <ModelView
+                   index={2}
+                   groupRef={large}
+                   gsapType='view2'
+                   controlRef={cameraControlLarge}
+                   setRotationState={setLargeRotation}
+                   item={model}
+                   size={size}/>
+
                 </div>
 
+                <Canvas
+                    className="w-full h-full"
+                    style={{
+                        position: "fixed",
+                        top: 0,
+                        botton: 0,
+                        left: 0,
+                        right: 0,
+                        overflow: 'hidden'
+                    }}
+                    eventSource={document.getElementById
+                        ('root')}
+                >
+                    <View.Port/>
+                </Canvas>
+            </div>
+            <div className="mx-auto w-full">
+                <p className="text-sm font-light text-center mb-5"> {model.title} </p>
+                <div className="flex-center">
+                    <ul className="color-container">
+                        {models.map((item,i) => (
+                            <li key={i}
+                            className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                            style={{
+                                backgroundColor: item.color[0]
+                            }}
+                            onClick={()=> setModel(item)} >
+
+                            </li>
+                        ))}
+                    </ul>
+                    <button className="size-btn-container">
+                        {sizes.map(({label, value})=>(
+                            <span key={label} className="size-btn"
+                            style={{backgroundColor: size === value ? 'white' 
+                        : 'transparent', color: size === value ? 'black' : 'white'}}
+                        onClick={()=> setSize(value)}>
+                                {label}
+                            </span>
+                        ))}
+                    </button>
+                </div>
             </div>
         </div>
     </section>
