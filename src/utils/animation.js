3e320ccee4ed7b0ebddfbb01642c04b93cc3e5d1
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-02 17:08:15 +0700

    Features section done

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,19 @@
+import gsap from "gsap"
+
+
+export const animateWithGsap = ( target, animationProps, scrollProps) => {
+    gsap.to(target, {
+        ...animationProps,
+        scrollTrigger:{
+            trigger: target,
+            toggleActions: 'restart reverse restart reverse',
+            start: 'top 85%',
+            ...scrollProps,
+        }
+    })
+}
+
+
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, 
     firstTarget, secondTarget, animationProps) => {
         timeline.to(rotationRef.current.rotation, {
