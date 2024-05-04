28ebe0119ba842f930c2e91a1ff2a741ba53481f
Aung Kaung Myint <aungkggmyint@gmail.com>
2024-05-04 14:25:34 +0700

    Footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 import Features from './components/Features'
 import HowItWork from "./components/HowItWork"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Model/>
       <Features/>
       <HowItWork/>
+      <Footer/>
     </main>
   )
 }
